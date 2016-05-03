6b3a8796fcbd61c02e838f183292bc6b49ddf180
Stanislav Osipov <work.osipov@yandex.ru>
2016-05-03 22:17:41 +0400

    binding behaviour example

--- src/app.js
+++ src/app.js
@@ -1,11 +1,3 @@
-import {BindingSignaler} from 'aurelia-templating-resources';
-import {inject} from 'aurelia-framework';
-
-@inject(BindingSignaler)
 export class App {
   message = 'Welcome to Aurelia!';
-
-  constructor(signaler) {
-  	setInterval(() => signaler.signal('my-signal'), 2000);
-  }
 }
