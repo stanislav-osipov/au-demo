2ea1577dcccf8e552519618f6568c6c38cd56735
Stanislav Osipov <Stanislav_Osipov@epam.com>
2016-05-06 11:36:21 +0400

    repeat example

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
 export class App {
   message = 'Welcome to Aurelia!';
+  squares = [];
+
+  randomColor() {
+  	var key = Math.floor(Math.random() * (666)) + 333; 
+  	return '#' + key;
+  }
+
+  created() {
+  	for (var i = 0; i < 10000; i++) {
+  		this.squares.push({color: this.randomColor()});
+  	}
+  }
 }
